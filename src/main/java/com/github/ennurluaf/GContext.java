ab302b4191366f1ab1855e2946056e74411e1c1e
Ennur Luaf <ennurluaf@gmail.com>
2025-08-10 19:26:55 +0200

    trying text bounds

--- src/main/java/com/github/ennurluaf/GContext.java
+++ src/main/java/com/github/ennurluaf/GContext.java
@@ -225,7 +225,7 @@ public class GContext {
 	public Dimension textPos(String text) {
 		FontMetrics fm = g.getFontMetrics();
 		int textWidth = fm.stringWidth(text);
-		int textHeight = fm.getHeight();
+		int textHeight = fm.getAscent() + fm.getDescent();
 		return new Dimension(textWidth, textHeight);
 	}
 
